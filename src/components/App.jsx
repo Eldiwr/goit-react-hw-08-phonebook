6bea34b911f8a53fe7894928ca9eb378382a033d
Eldiwr <Eldiwr97@gmail.com>
2022-09-20 19:14:23 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid';
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
