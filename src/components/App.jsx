8689a8afd9b0e3ee8544d4dbfbe534a384344199
Eldiwr <Eldiwr97@gmail.com>
2022-09-20 19:09:11 +0300

    LocalStorage - done
    Сделал LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,34 +4,18 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { Box } from './ContactForm/ContactForm.styled';
+import { useLocalStorage } from './hooks/LocalStorageHook';
 
 export const App = () => {
 
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useLocalStorage(('contacts' ?? [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ]));
 
   const [filter, setFilter] = useState('');
-
-
-
-  // componentDidMount() {
-  //   const savedContacts = localStorage.getItem("saved_contacts");
-  //   const parsedContacts = JSON.parse(savedContacts);
-  //   if (parsedContacts) {
-  //     this.setState({contacts: parsedContacts})
-  //   };
-  // };
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevState.contacts !== this.state.contacts) {
-  //     localStorage.setItem("saved_contacts", JSON.stringify(this.state.contacts));
-  //   };
-  //   console.log(this.state.contacts);
-  // };
 
   const handleOnSubmit = (name, number) => {
     
